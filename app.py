09860a02bafc117ef0961de3ba8efc0fa2cd2615
lavanya chakravarthy <lavanyachakravarthy003@gmail.com>
2026-07-13 22:30:16 +0530

    Updated Smart Email Classifier UI

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
-
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -23,5 +23,9 @@ def predict():
 
     return jsonify({"prediction": result})
 
+
+  
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
