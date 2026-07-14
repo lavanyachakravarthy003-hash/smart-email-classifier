9237c774832f8891e12d8a8713e632e4784fab66
lavanya chakravarthy <lavanyachakravarthy003@gmail.com>
2026-07-14 22:17:24 +0530

    Updated Smart Email Classifier

--- app.py
+++ app.py
@@ -1,31 +1,46 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 import pickle
 import os
-app = Flask(__name__)
+
+app = Flask(__name__, template_folder="frontend")
 CORS(app)
 
-model = pickle.load(open('model/spam_model.pkl', 'rb'))
-vectorizer = pickle.load(open('model/vectorizer.pkl', 'rb'))
+# Load trained model
+with open("model/spam_model.pkl", "rb") as f:
+    model = pickle.load(f)
 
-@app.route('/')
+with open("model/vectorizer.pkl", "rb") as f:
+    vectorizer = pickle.load(f)
+
+
+# Home Page
+@app.route("/")
 def home():
-    return "Smart Email Classifier Running!"
+    return render_template("index.html")
 
-@app.route('/predict', methods=['POST'])
+
+# Prediction API
+@app.route("/predict", methods=["POST"])
 def predict():
-    email_text = request.json['email']
-    
-    transformed = vectorizer.transform([email_text]).toarray()
-    prediction = model.predict(transformed)[0]
+    try:
+        data = request.get_json()
 
-    result = "Spam" if prediction == 1 else "Not Spam"
+        email_text = data["email"]
 
-    return jsonify({"prediction": result})
+        transformed = vectorizer.transform([email_text])
+
+        prediction = model.predict(transformed)[0]
+
+        result = "Spam" if prediction == 1 else "Not Spam"
+
+        return jsonify({"prediction": result})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
-  
-
+# Run server
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
